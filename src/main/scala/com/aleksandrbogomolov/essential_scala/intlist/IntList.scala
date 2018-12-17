66ad234ceabcf41121170d00743e9e3f0ba5740e
Aleksandr <ksandr.ua@gmail.com>
2018-12-17 21:45:09 +0300

    Renamed accumulator variable

--- src/main/scala/com/aleksandrbogomolov/essential_scala/intlist/IntList.scala
+++ src/main/scala/com/aleksandrbogomolov/essential_scala/intlist/IntList.scala
@@ -1,9 +1,9 @@
 package com.aleksandrbogomolov.essential_scala.intlist
 
 sealed trait IntList {
-  def fold[A](end: A, f: (Int, A) => A): A = this match {
-    case End => end
-    case Pair(hd, tl) => f(hd, tl.fold(end, f))
+  def fold[A](accum: A, f: (Int, A) => A): A = this match {
+    case End => accum
+    case Pair(hd, tl) => f(hd, tl.fold(accum, f))
   }
 
   def length: Int = fold(0, (_, tl) => 1 + tl)
